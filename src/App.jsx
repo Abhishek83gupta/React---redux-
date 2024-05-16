abbc42192bbe96ff142a1b6571b488d8b6bea5b2
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-05-16 23:54:56 +0530

    Redux done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from "./Components/AddTodo"
+import Todos from "./Components/Todos"
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-center">Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
